049aa002084913888d71d064b5c92518eb240ff5
ammar faris <ammarfaris37@gmail.com>
2017-08-05 18:04:15 +0800

    move android tabBar to bottom, add json static data source

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ export default class App extends Component {
     const MainNavigator = TabNavigator({
       ReactCourses: { screen: ReactCourses },
       ReactNativeCourses : { screen: ReactNativeCourses }
+    }, {
+      tabBarPosition: 'bottom'
     })
 
     return (
